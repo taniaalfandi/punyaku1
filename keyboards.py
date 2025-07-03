8b5e384ba333a6161f7a35f391f2d2980fbb537c
taniaalfandi <taniaalfandi@gmail.com>
2025-07-03 18:51:08 +0700

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,6 +1,6 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-def premium_menu():
-    markup = InlineKeyboardMarkup()
-    markup.add(InlineKeyboardButton("💎 Upgrade Premium", callback_data="upgrade_premium"))
-    return markup
+def premium_keyboard():
+    return InlineKeyboardMarkup().add(
+        InlineKeyboardButton("💎 Get Premium", callback_data="buy_premium")
+    )
