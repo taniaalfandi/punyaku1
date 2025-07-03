3977251448a8d379e5b416c046fdfdc64cf37fb8
taniaalfandi <taniaalfandi@gmail.com>
2025-07-03 18:48:51 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")  # Set in .env
-ADMINS = [123456789]                # Your admin ID
-MONGO_URI = "mongodb://localhost:27017"
-DB_NAME = "file_store_bot"
-CHANNEL_ID = -10012345678           # Your channel ID
+ADMINS = [6767172487]         # Your admin ID
+MONGO_URI = "mongodb+srv://upinputra30:RoOsNOLn8kDv8Vv8@upinputra.lzwpkfr.mongodb.net/?retryWrites=true&w=majority&appName=upinputra"
+DB_NAME = "upinputra30"
+CHANNEL_ID = -1002814433680        # Your channel ID
