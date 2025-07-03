bdb13bd6dacd1d52c25831984a9a3f6727732069
taniaalfandi <taniaalfandi@gmail.com>
2025-07-03 18:22:26 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BOT_TOKEN = os.getenv("7716770647:AAGrwnHN-7EiP7OQqOKtQi7OAMX5UHr_dS0")  # Simpan di environment variable
+ADMINS = [6767172487]  # Chat ID Admin
+MONGO_URI = "mongodb+srv://upinputra30:RoOsNOLn8kDv8Vv8@upinputra.lzwpkfr.mongodb.net/?retryWrites=true&w=majority&appName=upinputra"  # Ganti dengan URI MongoDB-mu
+DB_NAME = "upinputra30"
